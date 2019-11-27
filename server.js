7089310109f05ab51d2f8351edfbc0452bc565ad
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-27 11:22:20 +0530

    fixed typos

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Working"));
-const PORT = (process.env.PORT = 7000);
-app.listen(PORT, () => `Server running @ ${PORT}`);
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => console.log(`Server running @ ${PORT}`));
