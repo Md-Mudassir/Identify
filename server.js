ce35875285e6ab47d9067fbc303299cd0f0ea2cf
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-27 11:19:47 +0530

    added packages and server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Working"));
+const PORT = (process.env.PORT = 7000);
+app.listen(PORT, () => `Server running @ ${PORT}`);
