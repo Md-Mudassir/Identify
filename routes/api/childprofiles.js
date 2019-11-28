364163952fcec4ec2e62dddccb867ce3147d7a8b
Md-Mudassir <mdmudassirpro@gmail.com>
2019-11-28 18:00:12 +0530

    get all and delete route

--- routes/api/childprofiles.js
+++ routes/api/childprofiles.js
@@ -68,4 +68,60 @@ router.post("/", auth, upload.single("img"), async (req, res, next) => {
   }
 });
 
+//@route  GET api/childprofiles
+//@description get all childprofiles
+//@access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const childprofiles = await ChildProfile.find().sort({ date: -1 });
+    res.json(childprofiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//@route  GET api/childprofiles/:id
+//@description get childprofile by ID
+//@access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const childprofile = await ChildProfile.findById(req.params.id);
+    if (!childprofile) {
+      return res.status(404).json({ msg: "Child profile not found" });
+    }
+    res.json(childprofile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Child profile not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
+//@route  DELETE api/childprofiles/:id
+//@description Delete a child profile
+//@access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const childprofile = await ChildProfile.findById(req.params.id);
+    if (!childprofile) {
+      return res.status(404).json({ msg: "Child profile not found" });
+    }
+    //Check user
+    // if (childprofile.user.toString() !== req.user.id) {
+    //   return res.status(401).json({ msg: "User not authorized to delete" });
+    // }
+    await childprofile.remove();
+    res.json({ msg: "Child profile deleted" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Child profile not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
